275a783800954dbece5e525db3604e86606d7ba9
michalskidj66 <michalskidj66@gmail.com>
2017-04-23 19:15:23 -0400

    Add files via upload

---
+++ Methods.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DKChallenge
+{
+    class Methods
+    {
+        public List<double> searchContinuityAboveValue(List<double> data, int indexBegin, int indexEnd, double threshold, int winLength)
+        {
+            List<double> returnValue = new List<double>();
+            
+            
+            for (int i = indexBegin; i <= indexEnd; i++)
+            {
+                if (data[i] > threshold)
+                {
+                    double holder = data[i];
+                    if (returnValue.Contains(data.IndexOf(holder)))
+                    {
+                        returnValue.Add(data.IndexOf(holder, i, indexEnd));
+                    }
+                    else
+                    {
+                        returnValue.Add(data.IndexOf(holder));
+                    }
+              }
+            }
+
+            for (int j = 0; j < returnValue.Count(); j++)
+            {
+                if(returnValue[j] > indexEnd || returnValue[j] < indexBegin)
+                {
+                    double remover = returnValue[j];
+                    returnValue.Remove(remover);
+                }
+            }
+            int wlPlus = winLength + 1;
+            if (returnValue.Count() > winLength)
+            {
+                returnValue.RemoveRange(wlPlus, returnValue.Count() - wlPlus);
+                return returnValue;
+            }
+            else
+            {
+                return returnValue;
+            }
+        }
+
+        public List<double> backSearchContinuityWithinRange(List<double> data, int indexBegin, int indexEnd, double thresholdLo, double thresholdHi, int winLength)
+        {
+            List<double> returnValue = new List<double>();
+
+            for(int i = indexBegin; i > indexEnd; i--)
+            {
+                if (data[i] < thresholdHi && data[i] > thresholdLo)
+                {
+                    double holder = data[i];
+                    if (returnValue.Contains(data.IndexOf(holder)))
+                    {
+                        returnValue.Add(data.IndexOf(holder, i, indexEnd));
+                    }
+                    else
+                    {
+                        returnValue.Add(data.IndexOf(holder));
+                    }
+                }
+            }
+
+            for (int j = 0; j < returnValue.Count(); j++)
+            {
+                if (returnValue[j] < indexEnd || returnValue[j] > indexBegin)
+                {
+                    double remover = returnValue[j];
+                    returnValue.Remove(remover);
+                }
+            }
+            int wlPlus = winLength + 1;
+            if (returnValue.Count() > winLength)
+            {
+                returnValue.RemoveRange(wlPlus, returnValue.Count() - wlPlus);
+                return returnValue;
+            }
+            else
+            {
+                return returnValue;
+            }
+        }
+
+        public List<double> searchContinuityAboveValueTwoSignals(List<double> data1, List<double> data2, int indexBegin, int indexEnd, double threshold1, 
+            double threshold2, int winLength)
+        {
+            List<double> returnValue = new List<double>();
+
+            for (int i = indexBegin; i < indexEnd; i++)
+            {
+                if(data1[i] > threshold1 && data2[i] > threshold2)
+                {
+                    double holder = data1[i];
+                    if (returnValue.Contains(data1.IndexOf(holder)))
+                    {
+                        returnValue.Add(data1.IndexOf(holder, i, indexEnd));
+                    }
+                    else
+                    {
+                        returnValue.Add(data1.IndexOf(holder));
+                    }
+                }
+            }
+
+            for (int j = 0; j < returnValue.Count(); j++)
+            {
+                if (returnValue[j] > indexEnd || returnValue[j] < indexBegin)
+                {
+                    double remover = returnValue[j];
+                    returnValue.Remove(remover);
+                }
+            }
+            int wlPlus = winLength + 1;
+            if (returnValue.Count() > winLength)
+            {
+                returnValue.RemoveRange(wlPlus, returnValue.Count() - wlPlus);
+                return returnValue;
+            }
+            else
+            {
+                return returnValue;
+            }
+        }
+
+        public List<double> searchMultiContinuityWithinRange(List<double> data, int indexBegin, int indexEnd, double thresholdLo, double thresholdHi, int winLength)
+        {
+            List<double> returnValue = new List<double>();
+
+            for (int i = indexBegin; i < indexEnd; i++)
+            {
+                if(data[i] > thresholdLo && data[i] < thresholdHi)
+                {
+                    double holder = data[i];
+                    if (returnValue.Contains(data.IndexOf(holder)))
+                    {
+                        returnValue.Add(data.IndexOf(holder, i));
+                    }
+                    else
+                    {
+                        returnValue.Add(data.IndexOf(holder));
+                    }
+
+                }
+            }
+            for (int j = 0; j < returnValue.Count(); j++)
+            {
+                if (returnValue[j] > indexEnd || returnValue[j] < indexBegin)
+                {
+                    double remover = returnValue[j];
+                    returnValue.Remove(remover);
+                }
+            }
+           
+            double sequence = 0;
+            List<double> exclude = new List<double>();
+            for (int i = 0; i<returnValue.Count(); i++)
+            {
+                
+                if(returnValue[i] - (sequence + 1) == 0)
+                {
+                    sequence = returnValue[i];
+                    exclude.Add(returnValue[i]);
+                }
+                else if(i>0)
+                {
+                    exclude.Remove(returnValue[i - 1]);
+                }
+
+                sequence = returnValue[i];
+            }
+            foreach (var value in exclude)
+            {
+                returnValue.Remove(value);
+            }
+
+            int wlPlus = winLength * 2;
+            if (returnValue.Count() > winLength)
+            {
+                returnValue.RemoveRange(wlPlus, returnValue.Count() - wlPlus);
+                return returnValue;
+            }
+            else
+            {
+                return returnValue;
+            }
+
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DKChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> timeStamp = new List<double>();
+            List<double> ax = new List<double>();
+            List<double> ay = new List<double>();
+            List<double> az = new List<double>();
+            List<double> wx = new List<double>();
+            List<double> wy = new List<double>();
+            List<double> wz = new List<double>();
+
+            List<string> listA = new List<string>();
+            List<string> listB = new List<string>();
+            List<string> listC = new List<string>();
+            List<string> listD = new List<string>();
+            List<string> listE = new List<string>();
+            List<string> listF = new List<string>();
+            List<string> listG = new List<string>();
+
+            using (var fs = File.OpenRead(@"C:\Users\DJ\Desktop\latestSwing.csv"))
+            {
+                using (var reader = new StreamReader(fs))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        var values = line.Split(',');
+
+                        listA.Add(values[0]);
+                        listB.Add(values[1]);
+                        listC.Add(values[2]);
+                        listD.Add(values[3]);
+                        listE.Add(values[4]);
+                        listF.Add(values[5]);
+                        listG.Add(values[6]);
+
+
+                    }
+                }
+            }
+
+            for (int i = 0; i < listA.Count; i++)
+            {
+                timeStamp.Add(Convert.ToDouble(listA[i]));
+                ax.Add(Convert.ToDouble(listB[i]));
+                ay.Add(Convert.ToDouble(listC[i]));
+                az.Add(Convert.ToDouble(listD[i]));
+                wx.Add(Convert.ToDouble(listE[i]));
+                wy.Add(Convert.ToDouble(listF[i]));
+                wz.Add(Convert.ToDouble(listG[i]));
+
+            }
+
+            List<List<double>> theData = new List<List<double>>();
+
+            theData.Add(timeStamp);
+            theData.Add(ax);
+            theData.Add(ay);
+            theData.Add(az);
+            theData.Add(wx);
+            theData.Add(wy);
+            theData.Add(wz);
+
+            Methods m = new Methods();
+            List<double> scav = new List<double>();
+           scav = m.searchContinuityAboveValue(ax, 83, 140, 0, 3);
+           scav = m.backSearchContinuityWithinRange(ax, 1000, 23, 1, 1.453, 3);
+            scav = m.searchContinuityAboveValueTwoSignals(ax, ay, 23, 1000, 1, 1, 3);
+            scav = m.searchMultiContinuityWithinRange(ax, 23, 1000, 1, 1.453, 3); 
+        }
+            
+        
+       
+    }
+    
+}
